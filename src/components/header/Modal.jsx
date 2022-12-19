d19c9fc8eee20cd496f55c0a2e6dc03dcfb6ce33
John Ryu <jryu@Johns-MacBook-Pro.local>
2022-12-18 18:45:30 -0800

    modal fix, added closing modal when clicked outside of the modal window. Updated modal css

--- src/components/header/Modal.jsx
+++ src/components/header/Modal.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import "./modal.css";
 import Resume from "../../assets/resume_jy.pdf";
 
-// Call To Action Component
+// Call To Action buttons
 const Modal = ({ onRequestClose }) => {
-  // Use useEffect to add an event listener to the document
+  const ref = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      // Close the modal when clicked outside of the modal
+      onRequestClose();
+    }
+  };
+
   useEffect(() => {
     function onKeyDown(event) {
       if (event.keyCode === 27) {
@@ -16,11 +24,13 @@ const Modal = ({ onRequestClose }) => {
     // Prevent scolling
     document.body.style.overflow = "hidden";
     document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("click", handleClickOutside);
 
     // Clear things up when unmounting this component
     return () => {
       document.body.style.overflow = "visible";
       document.removeEventListener("keydown", onKeyDown);
+      document.removeEventListener("click", handleClickOutside);
     };
   });
 
@@ -28,19 +38,22 @@ const Modal = ({ onRequestClose }) => {
     <div className="modal__backdrop">
       <div className="modal__container">
         <h3 className="modal__title">John Ryu's Resume</h3>
-        <div className="modal__object">
-          {/* <object
-            data={Resume}
-            type="application/pdf"
-            aria-labelledby="jyresume"
-            width={"100%"}
-            height={"100%"}
-          ></object> */}
-          <iframe title="jyresume" src={Resume} frameborder="0"></iframe>
-        </div>
+        <iframe
+          id="resume_iframe"
+          title="jyresume"
+          src={Resume}
+          frameBorder="0"
+        >
+          <div className="modal__object"></div>
+        </iframe>
 
         <div className="modal__cta">
-          <a href="#home" className="modal__btn" onClick={onRequestClose}>
+          <a
+            ref={ref}
+            href="#home"
+            className="modal__btn"
+            onClick={onRequestClose}
+          >
             Close this modal
           </a>
           <a href={Resume} className="modal__btn" download>
