8e7523262b265233d3b8c0bd1adc608ea86e6ae8
smashyou <johnminryu@gmail.com>
2025-03-14 05:05:28 -0700

    updated modal.css and modal.jsk to optimize pdf viewer

--- src/components/header/Modal.jsx
+++ src/components/header/Modal.jsx
@@ -2,58 +2,48 @@ import React, { useEffect, useRef } from "react";
 import "./modal.css";
 import Resume from "../../assets/resume_jy.pdf";
 
-// Call To Action buttons
 const Modal = ({ onRequestClose }) => {
   const ref = useRef(null);
 
   const handleClickOutside = (event) => {
     if (ref.current && !ref.current.contains(event.target)) {
-      // Close the modal when clicked outside of the modal
       onRequestClose();
     }
   };
 
   useEffect(() => {
     function onKeyDown(event) {
-      if (event.keyCode === 27) {
-        // Close the modal when the Escape key is pressed
+      if (event.key === "Escape") {
         onRequestClose();
       }
     }
 
-    // Prevent scolling
     document.body.style.overflow = "hidden";
     document.addEventListener("keydown", onKeyDown);
-    document.addEventListener("click", handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
 
-    // Clear things up when unmounting this component
     return () => {
       document.body.style.overflow = "visible";
       document.removeEventListener("keydown", onKeyDown);
-      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  });
+  }, [onRequestClose]);
 
   return (
     <div className="modal__backdrop">
-      <div className="modal__container">
+      <div className="modal__container" ref={ref}>
         <h3 className="modal__title">John Ryu's Resume</h3>
         <div className="modal__object">
           <iframe
             id="resume_iframe"
             title="Resume"
-            src={`${Resume}#view=FitH`}
+            src={`${Resume}#toolbar=0&navpanes=0&scrollbar=0`}
           ></iframe>
         </div>
         <div className="modal__cta">
-          <a
-            ref={ref}
-            href="#home"
-            className="modal__btn"
-            onClick={onRequestClose}
-          >
-            Close this modal
-          </a>
+          <button className="modal__btn" onClick={onRequestClose}>
+            Close This Modal
+          </button>
           <a href={Resume} className="modal__btn" download>
             Download Resume
           </a>
