2eb07943d976f2216cd9b2496598add9a1c6beee
smashyou <johnminryu@gmail.com>
2025-03-29 09:11:21 -0700

    quick patch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -28,9 +28,6 @@ const Footer = () => {
           <a href="/#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="/#testimonials">Testimonials</a>
-        </li>
-        <li>
           <a href="/#contact">Contact</a>
         </li>
       </ul>
